0ccfdc9f4abe9689a968712f50efcdf798618b5d
Ibrahim Ahmed <96918810+IbrahimAhmed413@users.noreply.github.com>
2024-09-10 14:37:58 +0500

    Feat:Done All questions

--- exercise4.js
+++ exercise4.js
@@ -29,23 +29,105 @@ function showSelectedValue() {
 // Qno 2
 
 var phones = 
-        {
-            'iphone_se_red': {
-                'name': 'iPhone SE 64GB Red',
-                'price': '450',
-                'currency': 'US$',
-                'imageUrl': "https://www.mhr.pt/232729-large_default/apple-iphone-se-64gb-red.jpg"
-            },
-            'iphone_11_black': {
-                'name': 'iPhone 11 128GB Black',
-                'price': '869',
-                'currency': 'EUR',
-                'imageUrl': "https://assets.swappie.com/iphon11musta1-300x300.jpg"
-            },
-            'iphone_8_plus_silver': {
-                'name': 'iPhone 8 Plus 64GB Silver',
-                'price': '519',
-                'currency': 'US$',
-                'imageUrl': "https://www.mhr.pt/196170/apple-iphone-8-plus-64gb-silver.jpg"
-            }
-        };
+{
+    'iphone_se_red': {
+        'name': 'iPhone SE 64GB Red',
+        'price': '450',
+        'currency': 'US$',
+        'imageUrl': "./yCautEtLv0SBDJ2lzWrkdqpXkIeKd3nymuRfsiTJ.jpeg"
+    },
+    'iphone_11_black': {
+        'name': 'iPhone 11 128GB Black',
+        'price': '869',
+        'currency': 'EUR',
+        'imageUrl': "",
+    },
+    'iphone_8_plus_silver': {
+        'name': 'iPhone 8 Plus 64GB Silver',
+        'price': '519',
+        'currency': 'US$',
+        'imageUrl': "https://www.mhr.pt/196170/https://www.pinterest.com/pin/39758409202754321/visual-search/?surfaceType=flashlight-iphone-8-plus-64gb-silver.jpg"
+    }
+};
+
+
+
+
+function change(selectedPhone) {
+    
+    if (phones[selectedPhone]) {
+        // Update the product image
+        document.getElementById('productImage').src = phones[selectedPhone].imageUrl;
+        
+        // Update the product name
+        document.getElementById('productName').innerHTML = "<b>Product Name: " + phones[selectedPhone].name + "</b>";
+        
+        // Update the product price with 2 decimal places
+        document.getElementById('productPrice').innerHTML = "<b>Price: " + phones[selectedPhone].currency + " " + parseFloat(phones[selectedPhone].price).toFixed(2) + "</b>";
+    }
+}
+
+// Stop watch
+let hour = 0;
+let minute = 0;
+let second = 0;
+let count = 0;
+let timer = false;
+
+function startStop() {
+    if (!timer) {
+        timer = true;
+        document.getElementById('start').innerHTML = 'Stop';  // Change button text to 'Stop'
+        stopWatch();
+    } else {
+        timer = false;
+        document.getElementById('start').innerHTML = 'Start';  // Change button text back to 'Start'
+    }
+}
+
+function resetTimer() {
+    timer = false;
+    hour = 0;
+    minute = 0;
+    second = 0;
+    count = 0;
+    document.getElementById('hr').innerHTML = "00";
+    document.getElementById('min').innerHTML = "00";
+    document.getElementById('sec').innerHTML = "00";
+    document.getElementById('count').innerHTML = "00";
+    document.getElementById('start').innerHTML = 'Start';  // Reset button text to 'Start'
+}
+
+function stopWatch() {
+    if (timer) {
+        count++;
+
+        if (count == 100) {
+            second++;
+            count = 0;
+        }
+
+        if (second == 60) {
+            minute++;
+            second = 0;
+        }
+
+        if (minute == 60) {
+            hour++;
+            minute = 0;
+            second = 0;
+        }
+
+        let hrString = hour < 10 ? "0" + hour : hour;
+        let minString = minute < 10 ? "0" + minute : minute;
+        let secString = second < 10 ? "0" + second : second;
+        let countString = count < 10 ? "0" + count : count;
+
+        document.getElementById('hr').innerHTML = hrString;
+        document.getElementById('min').innerHTML = minString;
+        document.getElementById('sec').innerHTML = secString;
+        document.getElementById('count').innerHTML = countString;
+        
+        setTimeout(stopWatch, 10);  // Recursion to keep the timer running
+    }
+}
